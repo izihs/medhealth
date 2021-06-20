87cc1f5a92296b0a41732846db6d8adb7942a839
izihs <ihsanfaiz13@gmail.com>
2021-06-20 17:19:53 +0700

    Tugas Ke 8

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:medhealth/network/model/pref_profile_model.dart';
+import 'package:medhealth/pages/login_page.dart';
+import 'package:medhealth/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfilePages extends StatefulWidget {
   @override
@@ -6,11 +10,144 @@ class ProfilePages extends StatefulWidget {
 }
 
 class _ProfilePagesState extends State<ProfilePages> {
+  String fullName, createdDate, phone, email, address;
+  getPref() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      fullName = sharedPreferences.getString(PrefProfile.name);
+      createdDate = sharedPreferences.getString(PrefProfile.createdAt);
+      phone = sharedPreferences.getString(PrefProfile.phone);
+      email = sharedPreferences.getString(PrefProfile.email);
+      address = sharedPreferences.getString(PrefProfile.address);
+    });
+  }
+
+  signOut() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.remove(PrefProfile.idUser);
+    sharedPreferences.remove(PrefProfile.name);
+    sharedPreferences.remove(PrefProfile.email);
+    sharedPreferences.remove(PrefProfile.phone);
+    sharedPreferences.remove(PrefProfile.createdAt);
+
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPages()),
+        (route) => false);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPref();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Halaman Profile"),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "My Profile",
+                  style: regulerTextStyle.copyWith(fontSize: 24),
+                ),
+                InkWell(
+                  onTap: () {
+                    signOut();
+                  },
+                  child: Icon(
+                    Icons.exit_to_app,
+                    color: greenColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  fullName,
+                  style: boldTextStyle.copyWith(fontSize: 18),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Join at " + createdDate,
+                  style: lightTextStyle,
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: whiteColor,
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Phone",
+                  style: lightTextStyle,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  phone,
+                  style: boldTextStyle.copyWith(fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Email",
+                  style: lightTextStyle,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  email,
+                  style: boldTextStyle.copyWith(fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: whiteColor,
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Address",
+                  style: lightTextStyle,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  address,
+                  style: boldTextStyle.copyWith(fontSize: 18),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
